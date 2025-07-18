655dd4c1e529bfcf38d252ad44124c9174b0e744
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-07-18 15:42:13 +0100

    update the application

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,6 @@ module HozsystemApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
   end
 end

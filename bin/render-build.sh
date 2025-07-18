3780870a519536967486fd3139470d0f4d13afa1
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-07-18 15:05:28 +0100

    added the configurations

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
 set -o errexit
+
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bin/rails assets:precompile
+bin/rails assets:clean
+
+bin/rails db:migrate

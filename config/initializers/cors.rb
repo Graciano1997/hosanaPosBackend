1101991ca14857bee5045a55384880a5289adc36
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-07-19 12:03:15 +0100

    update the cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://hosannaposfront.onrender.com"
+    origins "https://hosanna-pos-front.vercel.app"
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
